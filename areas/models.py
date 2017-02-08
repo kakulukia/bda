3031fab16ad987ef524db6252dc33f1e2ee67043
Andy Grabow <andy@freilandkiwis.de>
2017-02-08 15:21:00 +0100

    working bar charts and sass

--- areas/models.py
+++ areas/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import Max
 
 
 class AreaBio(models.Model):
@@ -9,6 +10,9 @@ class AreaBio(models.Model):
     def __str__(self):
         return '{}, {}, {}'.format(self.name, self.age, self.country)
 
+    def max_space(self):
+        return self.entries.aggregate(Max('living_space'))['living_space__max']
+
 
 class BioEntry(models.Model):
     living_space = models.IntegerField()
@@ -17,3 +21,21 @@ class BioEntry(models.Model):
     year_to = models.IntegerField()
 
     area_bio = models.ForeignKey('areas.AreaBio', related_name='entries')
+
+    class Meta:
+        ordering = ['year_from']
+
+    def __str__(self):
+        return '{}-{}, {} in {} m²'.format(
+            self.year_from, self.year_to, self.number_of_people, self.living_space)
+
+    def years(self):
+        return (self.year_to - self.year_from) * 5
+
+    def percentage(self):
+        max = self.area_bio.max_space()
+        percentage = int(float(self.living_space) / float(max) * 100)
+        return percentage
+
+    def person_percentage(self):
+        return int(float(100) / float(self.number_of_people))
--- settings/common.py
+++ settings/common.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'compressor',
+    'django_extensions',
 
     'areas',
 ]
