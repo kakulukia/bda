38a73589a1ce4e71a6bf8a3f1b795cf3d37bdf10
Andy Grabow <andy@freilandkiwis.de>
2017-02-20 10:26:16 +0100

    js lebet außerhalb weiter Superagent ist mit am Start script werden am Ende geladen und ausgefuehrt

---
+++ assets/js/detail.js
@@ -0,0 +1,21 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    name: 'Andy',
+    age: '36',
+    country: 'Germany'
+  },
+  methods: {
+    updateBio: function () {
+      alert('hallo');
+    }
+  }
+
+});
+
+superagent.get('http://localhost:8000/api/area-bio/1/?format=json').end(function(err, res){
+  // Calling the end function will send the request
+  console.log('done2');
+  console.log(res);
+
+});
